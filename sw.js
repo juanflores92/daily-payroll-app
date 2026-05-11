368e3bfa5dd3f19f84e6b7d716681f4107417af3
Juan Flores <juanflores92@yahoo.com>
2026-05-11 21:05:36 +0000

    Fix: i18n was wiping Area <select> in worker rows; only translate thead now

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'payroll-v4';
+var CACHE_NAME = 'payroll-v5';
 var URLS_TO_CACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
